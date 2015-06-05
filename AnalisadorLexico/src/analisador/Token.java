521298fde1a0f60a2c9185bcf3d3c082e1ec5a5b
Caio-batista <caio.oliveira@ccc.ufcg.edu,br>
2015-06-05 20:43:24 -0300

    Analisador lexico funcionando completo

---
+++ AnalisadorLexico/src/analisador/Token.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analisador;
+
+/**
+ *
+ * @author Caio Batista
+ */
+public enum Token {
+    PALAVRA,OPERADOR_ARITMETICO, OPERADOR_LOGICO,OP_ATRIBUICAO, OP_RELACIONAL, OP_BOOLEANO, SEPARADOR,
+    ESPECIAL, COMENTARIO, ID, INT,NUMERO, ERROR, 
+}
